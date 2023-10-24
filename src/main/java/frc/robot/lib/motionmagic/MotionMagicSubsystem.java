81807477e78d5e2bde65d14837548154a43bd3f8
Wispy Sparks <wispysparks@gmail.com>
2023-10-24 16:17:36 -0500

    cleanup

--- src/main/java/frc/robot/lib/motionmagic/MotionMagicSubsystem.java
+++ src/main/java/frc/robot/lib/motionmagic/MotionMagicSubsystem.java
@@ -1,21 +1,18 @@
 package frc.robot.lib.motionmagic;
 
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
-
 public interface MotionMagicSubsystem {
 
-    public void configMotors(TalonFXConfiguration config);
-    public void configMotionMagic(double velocity, double acceleration);
-    public void stop();
-    public void setPosition(double position);
-    public double getSensorPosition();
-    public double getSensorTarget(); 
+    void configMotionMagic(double velocity, double acceleration);
+    void stop();
+    void setPosition(double position);
+    double getSensorPosition();
+    double getSensorTarget(); 
     /**
      * @param position SPECIFY UNITS
      * @param velocity SPECIFY UNITS
      * @param acceleration SPECIFY UNITS
      * @return A command that will tell the subsystem to go to a position with motion magic using the specified velocity and acceleration
      */
-    public MotionMagicCommand doMotionMagic(double position, double velocity, double acceleration);
+    MotionMagicCommand doMotionMagic(double position, double velocity, double acceleration);
     
 }
