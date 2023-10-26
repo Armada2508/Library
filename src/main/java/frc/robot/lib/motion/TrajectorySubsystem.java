f6314b91c9b1fdc4ae25879a4092c090f7390001
Wispy Sparks <wispysparks@gmail.com>
2023-10-26 14:55:50 -0500

    im dumb

--- src/main/java/frc/robot/lib/motion/TrajectorySubsystem.java
+++ src/main/java/frc/robot/lib/motion/TrajectorySubsystem.java
@@ -4,17 +4,12 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
-public interface TrajectorySubsystem { 
+public interface TrajectorySubsystem extends Subsystem { 
     
     Pose2d getPose();
     DifferentialDriveWheelSpeeds getWheelSpeeds();
     void setVelocity(double velocityL, double velocityR);
     void setVoltage(double voltsL, double voltsR);
-    // Extremely janky, so I don't have to write this method in my subsystems that use trajectories.
-    default Subsystem getRequirements() {
-        Object o = this;
-        return (Subsystem) o;
-    }
 }
 
 
