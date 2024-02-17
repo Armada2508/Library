30e49cc1091e08c84d9c60b759dcb19694bd6f3e
WispySparks <wispysparks@gmail.com>
2024-02-17 00:44:30 -0600

    play when disabled, allow other tunes

--- src/main/java/frc/robot/lib/music/TalonMusic.java
+++ src/main/java/frc/robot/lib/music/TalonMusic.java
@@ -1,10 +1,12 @@
 package frc.robot.lib.music;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import com.ctre.phoenix6.Orchestra;
+import com.ctre.phoenix6.configs.AudioConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.Filesystem;
@@ -14,23 +16,38 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 public class TalonMusic {
 
     private static final Orchestra orchestra = new Orchestra();
-    private static final String startupSong = Filesystem.getDeployDirectory().getAbsolutePath() + "/PacmanMelody.chrp";
+    private static final AudioConfigs audioConfigs = new AudioConfigs().withAllowMusicDurDisable(true);
     private static final List<Subsystem> subsystems = new ArrayList<>();
+    private static final String startupSong = "PacmanMelody.chrp";
 
+    /**
+     * Adds talons to the class's internal orchestra, also modifies their audio configs to allow music when disabled.
+     * @param currentSubsystem to add for music requirements
+     * @param talons to use in orchestra
+     */
     public static void addTalonFX(Subsystem currentSubsystem, TalonFX... talons) {
         Objects.requireNonNull(currentSubsystem);
         Objects.requireNonNull(talons);
         for (TalonFX talonFX : talons) {
+            talonFX.getConfigurator().apply(audioConfigs);
             orchestra.addInstrument(talonFX);
             subsystems.add(currentSubsystem);
         }
     }
     
     public static void playStartupTune() {
+        playTune(startupSong);
+    }
+
+    /**
+     * Plays a .chrp file on the talonFXs that have been added to the orchestra.
+     * @param fileName file to play, we assume it's in the deploy directory
+     */
+    public static void playTune(String fileName) {
         if (orchestra.isPlaying()) {
             orchestra.stop();
         }
-        orchestra.loadMusic(startupSong);
+        orchestra.loadMusic(Filesystem.getDeployDirectory().getAbsolutePath() + File.separator + fileName);
         Subsystem[] requirements = subsystems.toArray(new Subsystem[subsystems.size()]);
         Commands.runOnce(orchestra::play, requirements)
             .andThen(Commands.waitUntil(() -> !orchestra.isPlaying()))
