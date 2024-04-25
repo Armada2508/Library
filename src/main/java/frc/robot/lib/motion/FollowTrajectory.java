d53c2951e1ccb0f4e97acbc8dddef8b5a5d1d85d
Wispy Sparks <wispysparks@gmail.com>
2024-04-25 14:49:12 -0500

    delete unused classes, simplify FollowTrajectory

--- src/main/java/frc/robot/lib/motion/FollowTrajectory.java
+++ src/main/java/frc/robot/lib/motion/FollowTrajectory.java
@@ -1,6 +1,5 @@
 package frc.robot.lib.motion;
 
-import java.util.ArrayList;
 import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 
@@ -10,19 +9,10 @@ import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.trajectory.constraint.CentripetalAccelerationConstraint;
-import edu.wpi.first.math.trajectory.constraint.DifferentialDriveKinematicsConstraint;
-import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -36,10 +26,6 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
  * Helper class to generate commands for following trajectories
  */
 public class FollowTrajectory {
-
-    private static final NetworkTable debugTable = NetworkTableInstance.getDefault().getTable("ramsete");
-    private static final NetworkTableEntry leftReference = debugTable.getEntry("left_reference");
-    private static final NetworkTableEntry rightReference = debugTable.getEntry("right_reference");
 
     private static SimpleMotorFeedforward feedForward;
     private static DifferentialDriveKinematics diffKinematics;
@@ -89,24 +75,20 @@ public class FollowTrajectory {
      * @param zeroPose The position to start relative to
      * @return Returns a RamseteCommand that will follow the specified trajectory with the specified driveSubsystem
      */
-    public static Command getCommandFeedforward(TrajectorySubsystem driveSubsystem, Trajectory trajectory, Pose2d zeroPose) {
+    public static Command getCommandFeedforward(Trajectory trajectory, Pose2d zeroPose, Supplier<Pose2d> poseSupplier, Supplier<DifferentialDriveWheelSpeeds> wheelSpeeds, BiConsumer<Double, Double> voltageConsumer, Subsystem driveSubsystem) {
         trajectory = trajectory.relativeTo(zeroPose);
         PIDController leftController = new PIDController(0, 0, 0);
         PIDController rightController = new PIDController(0, 0, 0);
         return new RamseteCommand(
                 trajectory,
-                driveSubsystem::getPose,
+                poseSupplier,
                 disabledController,
                 feedForward,
                 diffKinematics,
-                driveSubsystem::getWheelSpeeds,
+                wheelSpeeds,
                 leftController,
                 rightController,
-                (voltsL, voltsR) -> {
-                    driveSubsystem.setVoltage(voltsL, voltsR);
-                    leftReference.setNumber(leftController.getSetpoint());
-                    rightReference.setNumber(rightController.getSetpoint());
-                },
+                voltageConsumer,
                 driveSubsystem);
     }
 
@@ -117,22 +99,18 @@ public class FollowTrajectory {
      * @param zeroPose The position to start relative to
      * @return Returns a RamseteCommand that will follow the specified trajectory with the specified driveSubsystem
      */
-    public static Command getCommand(TrajectorySubsystem driveSubsystem, Trajectory trajectory, Pose2d zeroPose) {
+    public static Command getCommand(Trajectory trajectory, Pose2d zeroPose, Supplier<Pose2d> poseSupplier, Supplier<DifferentialDriveWheelSpeeds> wheelSpeeds, BiConsumer<Double, Double> voltageConsumer, Subsystem driveSubsystem) {
         trajectory = trajectory.relativeTo(zeroPose);
         return new RamseteCommand(
                 trajectory,
-                driveSubsystem::getPose,
+                poseSupplier,
                 ramseteController,
                 feedForward,
                 diffKinematics,
-                driveSubsystem::getWheelSpeeds,
+                wheelSpeeds,
                 leftPidController,
                 rightPidController,
-                (voltsL, voltsR) -> {
-                    driveSubsystem.setVoltage(voltsL, voltsR);
-                    leftReference.setNumber(leftPidController.getSetpoint());
-                    rightReference.setNumber(rightPidController.getSetpoint());
-                },
+                voltageConsumer,
                 driveSubsystem);
     }
 
@@ -143,14 +121,14 @@ public class FollowTrajectory {
      * @param zeroPose The position to start relative to
      * @return Returns a RamseteCommand that will follow the specified trajectory with the specified driveSubsystem
      */
-    public static Command getCommandTalon(Trajectory trajectory, Pose2d zeroPose, Supplier<Pose2d> pose, BiConsumer<Double, Double> velocity, Subsystem driveSubsystem) {
+    public static Command getCommandTalon(Trajectory trajectory, Pose2d zeroPose, Supplier<Pose2d> poseSupplier, BiConsumer<Double, Double> velocityConsumer, Subsystem driveSubsystem) {
         trajectory = trajectory.transformBy(new Transform2d(new Pose2d(), zeroPose));
         return new RamseteCommand(
                 trajectory,
-                pose::get,
+                poseSupplier,
                 ramseteController,
                 diffKinematics,
-                velocity::accept,
+                velocityConsumer,
                 driveSubsystem);
     }
 
@@ -174,72 +152,4 @@ public class FollowTrajectory {
         }).until(() -> timer.hasElapsed(trajectory.getTotalTimeSeconds())));
     }
     
-    /**
-     * Returns a RamseteCommand that follows a generated trajectory using no feedback
-     * @param driveSubsystem The DriveSubsystem to use
-     * @param start The position to start at
-     * @param end The position to end at
-     * @param maxVelocity The maximum velocity of the robot
-     * @param maxAcceleration The maximum acceleration of the robot
-     * @param maxVoltage The maximum voltage that can be applied to the motors
-     * @param maxCentripetalAccleration The maximum centripetal acceleration of the robot
-     * @param reversed If the trajectory should be reversed
-     * @return Returns a RamseteCommand that will follow the specified trajectory with the specified driveSubsystem
-     */
-    public static Command getCommandFeedforward(TrajectorySubsystem driveSubsystem, Pose2d start, Pose2d end, double maxVelocity, double maxAcceleration, double maxVoltage, double maxCentripetalAccleration, boolean reversed) {
-        TrajectoryConfig config = new TrajectoryConfig(maxVelocity, maxAcceleration);
-        config.setReversed(reversed);
-        config.addConstraint(new DifferentialDriveKinematicsConstraint(diffKinematics, maxVelocity));
-        config.addConstraint(new DifferentialDriveVoltageConstraint(feedForward, diffKinematics, maxVoltage));
-        config.addConstraint(new CentripetalAccelerationConstraint(maxAcceleration));
-        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(start, new ArrayList<Translation2d>(), end, config);
-        trajectory = trajectory.relativeTo(trajectory.getInitialPose());
-        return getCommandFeedforward(driveSubsystem, trajectory, trajectory.getInitialPose());
-    }
-
-    /**
-     * Returns a RamseteCommand that follows a generated trajectory
-     * @param driveSubsystem The DriveSubsystem to use
-     * @param start The position to start at
-     * @param end The position to end at
-     * @param maxVelocity The maximum velocity of the robot
-     * @param maxAcceleration The maximum acceleration of the robot
-     * @param maxVoltage The maximum voltage that can be applied to the motors
-     * @param maxCentripetalAccleration The maximum centripetal acceleration of the robot
-     * @param reversed If the trajectory should be reversed
-     * @return Returns a RamseteCommand that will follow the specified trajectory with the specified driveSubsystem
-     */
-    public static Command getCommand(TrajectorySubsystem driveSubsystem, Pose2d start, Pose2d end, double maxVelocity, double maxAcceleration, double maxVoltage, double maxCentripetalAccleration, boolean reversed) {
-        TrajectoryConfig config = new TrajectoryConfig(maxVelocity, maxAcceleration);
-        config.setReversed(reversed);
-        config.addConstraint(new DifferentialDriveKinematicsConstraint(diffKinematics, maxVelocity));
-        config.addConstraint(new DifferentialDriveVoltageConstraint(feedForward, diffKinematics, maxVoltage));
-        config.addConstraint(new CentripetalAccelerationConstraint(maxCentripetalAccleration));
-        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(start, new ArrayList<Translation2d>(), end, config);
-        trajectory = trajectory.relativeTo(trajectory.getInitialPose());
-        return getCommand(driveSubsystem, trajectory, trajectory.getInitialPose());
-    }
-
-    /**
-     * Returns a RamseteCommand that follows a generated trajectory and uses the PID loop on the Talons
-     * @param driveSubsystem The DriveSubsystem to use
-     * @param start The position to start at
-     * @param end The position to end at
-     * @param maxVelocity The maximum velocity of the robot
-     * @param maxAcceleration The maximum acceleration of the robot
-     * @param maxVoltage The maximum voltage that can be applied to the motors
-     * @param maxCentripetalAccleration The maximum centripetal acceleration of the robot
-     * @param reversed If the trajectory should be reversed
-     * @return Returns a RamseteCommand that will follow the specified trajectory with the specified driveSubsystem
-     */
-    public static Command getCommandTalon(TrajectorySubsystem driveSubsystem, Pose2d start, Pose2d end, double maxVelocity, double maxAcceleration, double maxVoltage, double maxCentripetalAccleration, boolean reversed) {
-        TrajectoryConfig config = new TrajectoryConfig(maxVelocity, maxAcceleration);
-        config.setReversed(reversed);
-        config.addConstraint(new DifferentialDriveKinematicsConstraint(diffKinematics, maxVelocity));
-        config.addConstraint(new DifferentialDriveVoltageConstraint(feedForward, diffKinematics, maxVoltage));
-        config.addConstraint(new CentripetalAccelerationConstraint(maxCentripetalAccleration));
-        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(start, new ArrayList<Translation2d>(), end, config);
-        trajectory = trajectory.relativeTo(trajectory.getInitialPose());
-        return getCommandTalon(trajectory, trajectory.getInitialPose(), driveSubsystem::getPose, driveSubsystem::setVelocity, driveSubsystem);
-    }
 } 
