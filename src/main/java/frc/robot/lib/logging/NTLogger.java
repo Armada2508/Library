27ea4b69f05b9e7fa5bf9c4c83ffb9c2ed910059
Wispy Sparks <wispysparks@gmail.com>
2024-04-25 13:50:32 -0500

    protobufs, one logging map

--- src/main/java/frc/robot/lib/logging/NTLogger.java
+++ src/main/java/frc/robot/lib/logging/NTLogger.java
@@ -19,11 +19,15 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.geometry.Twist2d;
 import edu.wpi.first.math.geometry.Twist3d;
+import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.ProtobufPublisher;
+import edu.wpi.first.networktables.ProtobufTopic;
 import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.networktables.StructTopic;
+import edu.wpi.first.util.protobuf.Protobuf;
 import edu.wpi.first.util.struct.Struct;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -34,6 +38,7 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.WrapperCommand;
+import us.hebi.quickbuf.ProtoMessage;
 
 /**
  * Used to log fields to network tables and data log for viewing during testing and after a match. <p>
@@ -48,8 +53,9 @@ public final class NTLogger {
     private static NetworkTable mainTable = NetworkTableInstance.getDefault().getTable("Logging");
     private static NetworkTable schedulerTable = mainTable.getSubTable("Command Scheduler");
     private static Map<Loggable, Integer> indexedLoggables = new HashMap<>();
-    private static Map<Loggable, Map<String, Object>> loggingMaps = new HashMap<>();
+    private static Map<String, Object> loggingMap = new HashMap<>();
     private static List<StructPublisher<?>> structPublishers = new ArrayList<>();
+    private static List<ProtobufPublisher<?>> protobufPublishers = new ArrayList<>();
 
     private NTLogger() {}
 
@@ -71,11 +77,17 @@ public final class NTLogger {
         indexedLoggables.forEach((loggable, index) -> {
             NetworkTable table = (index == 0) ? mainTable.getSubTable(loggable.getClass().getSimpleName()) : 
                 mainTable.getSubTable(loggable.getClass().getSimpleName() + "-" + index);
-            loggable.log(loggingMaps.get(loggable)).forEach((name, val) -> {
+            loggingMap.clear();
+            loggable.log(loggingMap).forEach((name, val) -> {
                 if (name == null || val == null) return;
                 Optional<Struct<Object>> struct = getStruct(val);
+                Optional<Protobuf<Object, ProtoMessage<?>>> protobuf = getProtobuf(val);
                 if (struct.isPresent()) {
                     logStruct(table, name, val, struct.get());
+                    return;
+                }
+                if (protobuf.isPresent()) {
+                    logProtobuf(table, name, val, protobuf.get());
                     return;
                 }
                 NetworkTableEntry entry = table.getEntry(name);
@@ -99,7 +111,6 @@ public final class NTLogger {
             .collect(Collectors.toList())
             .size();
         indexedLoggables.put(obj, index);
-        loggingMaps.put(obj, new HashMap<>());
     }
 
     /**
@@ -109,7 +120,7 @@ public final class NTLogger {
      * @param map to fill
      * @return the map passed in for method chaining
      */
-    public static Map<String, Object> getTalonLog(TalonFX talon, String name, Map<String, Object> map) {
+    public static Map<String, Object> putTalonLog(TalonFX talon, String name, Map<String, Object> map) {
         map.put(name + ": Device ID", talon.getDeviceID());
         map.put(name + ": Control Mode", talon.getControlMode().getValue().toString());
         map.put(name + ": Rotor Polarity", talon.getAppliedRotorPolarity().getValue().name());
@@ -135,9 +146,9 @@ public final class NTLogger {
      * @param map to fill
      * @return the map passed in for method chaining
      */
-    public static Map<String, Object> getTalonLog(TalonFX talon, Map<String, Object> map) {
+    public static Map<String, Object> putTalonLog(TalonFX talon, Map<String, Object> map) {
         int ID = talon.getDeviceID();
-        return getTalonLog(talon, "TalonFX " + ID, map);
+        return putTalonLog(talon, "TalonFX " + ID, map);
     }
     
     /**
@@ -161,10 +172,10 @@ public final class NTLogger {
                 commandGroupCurrentCommand += c.getName() + " ";
             }
         }
-        map.put("_Name", subsystem.getName());
-        map.put("Default Command", subsystem.getDefaultCommand() == null ? "None" : subsystem.getDefaultCommand().getName());
-        map.put("Current Command", currentCommand == null ? "None" : currentCommand.getName());
-        map.put("Command Group Current Command", commandGroupCurrentCommand);
+        map.put("Subsystem: _Name", subsystem.getName());
+        map.put("Subsystem: Default Command", subsystem.getDefaultCommand() == null ? "None" : subsystem.getDefaultCommand().getName());
+        map.put("Subsystem: Current Command", currentCommand == null ? "None" : currentCommand.getName());
+        map.put("Subsystem: Command Group Current Command", commandGroupCurrentCommand);
         return map;
     }
 
@@ -208,6 +219,39 @@ public final class NTLogger {
         if (obj instanceof Twist2d) struct = Twist2d.struct;
         if (obj instanceof Twist3d) struct = Twist3d.struct;
         return Optional.ofNullable((Struct<T>) struct);
+    }
+
+    /**
+     * Logs a value that uses protobufs to network tables. 
+     * @param table network table to log value to
+     * @param name for value
+     * @param objToLog object that will be logged as a protobuf
+     * @param protobuf implementation that will be used for network tables to log object
+     */
+    @SuppressWarnings("unchecked")
+    private static <T, U extends ProtoMessage<?>> void logProtobuf(NetworkTable table, String name, T objToLog, Protobuf<T, U> protobuf) {
+        ProtobufTopic<T> topic = table.getProtobufTopic(name, protobuf);
+        for (ProtobufPublisher<?> publisher : protobufPublishers) {
+            if (publisher.getTopic().equals(topic)) {
+                ((ProtobufPublisher<T>) publisher).set(objToLog);
+                return;
+            }
+        }
+        ProtobufPublisher<T> publisher = topic.publish();
+        publisher.set(objToLog);    
+        protobufPublishers.add(publisher);
+    }
+
+    /**
+     * Gets the corresponding protobuf implementation for an object e.g. Pose2d.proto for a Pose2d.
+     * @param obj object to get struct for
+     * @return An object's protobuf implementation or empty if there is none
+     */
+    @SuppressWarnings("unchecked")
+    private static <T, U extends ProtoMessage<?>> Optional<Protobuf<T, U>> getProtobuf(T obj) {
+        Protobuf<?, ?> protobuf = null;
+        if (obj instanceof Trajectory) protobuf = Trajectory.proto;
+        return Optional.ofNullable((Protobuf<T, U>) protobuf);
     }
 
     /**
